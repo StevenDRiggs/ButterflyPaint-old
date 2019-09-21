9f4b81c59e995da594e749798104bc4f19d67575
JaxSnow <sfriggs21@gmail.com>
2019-09-21 08:13:34 -0600

    splash page timer working

---
+++ bpaint/models.py
@@ -0,0 +1,17 @@
+from bpaint import db
+
+
+class Color(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    medium = db.Column(db.String(2), nullable=False)
+    # color_num = db.Column(db.Integer, nullable=False)
+    name = db.Column(db.String(50), unique=True)
+    pure = db.Column(db.Boolean, default=False, nullable=False)
+    recipe = db.Column(db.Text, nullable=False, unique=True)
+    image = db.Column(db.String(25), nullable=False, unique=True)
+
+    def __repr__(self):
+        if self.name:
+            return f'{self.color_num}. {self.name}'
+        else:
+            return f'{self.color_num}. {self.recipe}'
--- bpaint/splash/routes.py
+++ bpaint/splash/routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, redirect, render_template, url_for
 
-bp = Blueprint('splash', __name__, template_folder='templates')
+bp = Blueprint('splash', __name__, template_folder='templates', static_folder='static', static_url_path='/splash/static')
 
 splash_seen = False
 
@@ -9,7 +9,7 @@ splash_seen = False
 def splash():
     global splash_seen
     if not splash_seen:
-        splash_seen = True
+        # splash_seen = True
         return render_template('splash/splash.html')
     else:
         return redirect(url_for('splash.index'))
