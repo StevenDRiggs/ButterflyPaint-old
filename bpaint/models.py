ef5b3b3f0df770b82f123261b84c057db9b2eb08
JaxSnow <sfriggs21@gmail.com>
2019-10-28 22:25:58 -0600

    db_add corrected, start db_add counter functionality

--- bpaint/models.py
+++ bpaint/models.py
@@ -1,10 +1,10 @@
 from bpaint import db
 
 
-link = db.Table(
-    db.Column('color_id', db.Integer, db.ForeignKey('color.id')),
-    db.Column('recipe_id', db.Integer, db.ForeignKey('color.id')),
-    db.Column('quantity', db.Integer)
+link = db.Table('link',
+    db.Column('color_id', db.Integer, db.ForeignKey('color.id'), primary_key=True),
+    db.Column('recipe_id', db.Integer, db.ForeignKey('color.id'), primary_key=True),
+    # db.Column('quantity', db.Integer)
 )
 
 
@@ -13,7 +13,10 @@ class Color(db.Model):
     medium = db.Column(db.String(2), nullable=False)
     name = db.Column(db.String(50), nullable=False, unique=True)
     pure = db.Column(db.Boolean, default=False, nullable=False)
-    recipe = db.relationship('Color', secondary=link)
+    recipe = db.relationship('Color',
+            secondary=link,
+            primaryjoin=id==link.c.color_id,
+            secondaryjoin=id==link.c.recipe_id)
     swatch = db.Column(db.String(25), nullable=False, unique=True)
 
     def __repr__(self):
