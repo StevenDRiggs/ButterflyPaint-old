c28124f751605825ff702c4db339a3233bb26661
StevenDRiggs <sfriggs21@gmail.com>
2019-11-03 02:20:16 -0700

    models structure correct except cascades

--- bpaint/models.py
+++ bpaint/models.py
@@ -8,19 +8,39 @@ class Recipe(db.Model):
     ingredients = db.relationship('Color')
     quantity = db.Column(db.Integer)
 
+    def __init__(self, ingredient, quantity):
+        print(f'{self.base_id=}\n{self.ingredients=}\n{self.quantity=}\n{ingredient=}\n{quantity=}')
+        self.ingredients = ingredient
+        self.quantity = quantity
+
     def __repr__(self):
-        return f'Recipe({self.base_id=}, {self.ingredients=}, {self.quantity=})'
+        return f'{self.ingredients} x{self.quantity}'
 
 class Color(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     medium = db.Column(db.String(2), nullable=False)
     name = db.Column(db.String(50), nullable=False, unique=True)
     pure = db.Column(db.Boolean, default=False, nullable=False)
-    recipe = db.relationship(Recipe,
-            primaryjoin=id==Recipe.base_id)
+    _recipe = db.relationship(Recipe,
+            primaryjoin=id==Recipe.base_id,
+            join_depth=1)
+    recipe = association_proxy('_recipe', 'ingredients',
+        creator = lambda _: _)
     swatch = db.Column(db.String(25), nullable=False, unique=True)
 
+    def __init__(self, medium, name, pure, recipe=None, *, swatch):
+        self.medium = medium
+        self.name = name
+        self.pure = pure
+        self.swatch = swatch
+
+        if self.pure:
+            self.recipe = [Recipe(self, 1)]
+        else:
+            for ingredient, quantity in recipe:
+                self.recipe.append(Recipe(ingredient, quantity))
+
     def __repr__(self):
-        return f'Color({self.name=} {self.recipe=})'
+        return f'Color({self.name})'
 
 
