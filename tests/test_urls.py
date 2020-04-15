e83940845cf627390a13bc9894771526c7cef3b6
Steven Riggs <sfriggs21@gmail.com>
2020-04-15 00:28:52 -0600

    continue expanding test_urls.py, fix failing tests after all url tests added

--- tests/test_urls.py
+++ tests/test_urls.py
@@ -1,5 +1,7 @@
 import unittest
 
+from random import randint
+
 from bpaint import app
 
 
@@ -22,17 +24,44 @@ class TestValidURLs(unittest.TestCase):
         pass
 
 
+    def status_code_check(self, address, code=200, **kwargs):
+        result = self.app.get(address, **kwargs)
+        self.assertEqual(result.status_code, code)
+
+
     def test_splash_status_code(self):
-        result = self.app.get('/')
-        self.assertEqual(result.status_code, 200)
+        self.status_code_check('/')
 
     def test_index_status_code(self):
-        result = self.app.get('/index')
-        self.assertEqual(result.status_code, 200)
+        self.status_code_check('/index')
 
     def test_database_status_code(self):
-        result = self.app.get('/database')
-        self.assertEqual(result.status_code, 200)
+        self.status_code_check('/database')
+
+    def test_database_add_status_code(self):
+        self.status_code_check('/database/add')
+
+    def test_database_add_single_status_code(self):
+        self.status_code_check('/database/add/single')
+
+    def test_database_add_batch_status_code(self):
+        self.status_code_check('/database/add/batch')
+
+    def test_database_update_status_code(self):
+        self.status_code_check('/database/update')
+
+    def test_database_update_single_status_code(self):
+        self.status_code_check('/database/update/single')
+
+    def test_database_update_single_color_idstatus_code(self):
+        color_id = randint(1, 5)
+        self.status_code_check(f'/database/update/single/{color_id}')
+
+    def test_database_update_batch_status_code(self):
+        self.status_code_check('/database/update/batch')
+
+    def test_database_update_batch_update_status_code(self):
+        self.status_code_check('/database/update/batch/update', choices=[('a', 1), ('b', 2), ('c', 3)])
 
 
 if __name__ == '__main__':
