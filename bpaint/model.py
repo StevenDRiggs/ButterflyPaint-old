589b57898a9df99b37ad09a0f8effe0bbc0c3616
StevenDRiggs <sfriggs21@gmail.com>
2020-05-31 20:00:10 -0600

    added relationships to models; added .swp files to .gitignore

--- bpaint/model.py
+++ bpaint/model.py
@@ -10,8 +10,10 @@ from werkzeug.security import generate_password_hash as gph, check_password_hash
 db = SQLAlchemy()
 
 class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
+    name = db.Column(db.String, nullable=False, unique=True)
     email = db.Column(db.String)
     password = db.Column(db.String, nullable=False)
 
@@ -24,20 +26,33 @@ class User(db.Model, UserMixin):
         
 
 class Color(db.Model):
+    __tablename__ = 'colors'
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     manufacturer = db.Column(db.String)
     image_url = db.Column(db.String, nullable=False)
 
+    inventory = db.relationship('Inventory', uselist=False, lazy=True)
+
+    recipe = db.relationship('Color',
+            secondary='recipes',
+            primaryjoin='colors.c.id==recipes.c.parent_color_id',
+            secondaryjoin='colors.c.id==recipes.c.ingredient_color_id'
+    )
+
 
 class Recipe(db.Model):
+    __tablename__ = 'recipes'
+
     id = db.Column(db.Integer, primary_key=True)
-    parent_color_id = db.Column(db.Integer)
-    ingredient_color_id = db.Column(db.Integer)
+    parent_color_id = db.Column(db.Integer, db.ForeignKey('colors.id'))
+    ingredient_color_id = db.Column(db.Integer, db.ForeignKey('colors.id'))
     quantity = db.Column(db.Integer, nullable=False)
 
 
 class Inventory(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    color_id = db.Column(db.Integer, db.ForeignKey('colors.id'))
     in_stock = db.Column(db.Integer, default=0)
     minimum_stock = db.Column(db.Integer, default=-1)
